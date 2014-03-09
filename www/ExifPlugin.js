db049060b239f48f48348da5f0eeeb733804929d
Paulo Vitor Magacho da Silva <pvmagacho@gmail.com>
2014-03-09 04:19:51 -0300

    Fix javascript.

--- www/ExifPlugin.js
+++ www/ExifPlugin.js
@@ -1,6 +1,49 @@
-window.exif = function(action, arr, callback, error) {
-    cordova.exec(callback, error, "Exif", action, arr);
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+*/
+
+var argscheck = require('cordova/argscheck'),
+    exec = require('cordova/exec'),
+    ExifPlugin = require('./ExifPlugin');
+
+var exifExport = {};
+
+/**
+ * Set location in JPEG Exif.
+ *
+ * @param {Function} successCallback
+ * @param {Function} errorCallback
+ * @param {Object} options
+ */
+exifExport.setLocation = function(successCallback, errorCallback, options) {
+    argscheck.checkArgs('fFO', 'ExifPlugin.setLocation', arguments);
+    options = options || {};
+    var getValue = argscheck.getValue;
+
+    var fileName = getValue(options.fileName, "");
+    var latitude = getValue(options.latitude, "NaN");
+    var longitude = getValue(options.longitude, "NaN");
+
+    var args = [fileName, latitude, longitude];
+
+    exec(successCallback, errorCallback, "Exit", "setlocation", args);
 };
-window.wifiinfo  = function(callback, error) {
-    cordova.exec(callback, error, 'Exif', 'wifiinfo', [] );
-};
+
+module.exports = exifExport;
